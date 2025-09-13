08d4f3549cceb729fde64c393ac81e157ddca922
Liyobor <a0963382999@gmail.com>
2025-09-14 01:08:10 +0800

    add home page

--- lib/home/logic.dart
+++ lib/home/logic.dart
@@ -1,5 +1,19 @@
+import 'package:encrypt_shared_preferences/provider.dart';
 import 'package:get/get.dart';
 
 class HomeLogic extends GetxController {
 
+  var account = "unknown".obs;
+
+  @override
+  void onInit() {
+    _loadAccount().then((onValue) => account(onValue));
+    super.onInit();
+  }
+
+  Future<String> _loadAccount() async {
+    final prefs = EncryptedSharedPreferencesAsync.getInstance();
+    final acc = await prefs.getString('account');
+    return acc ?? 'unknown';
+  }
 }
--- lib/home/view.dart
+++ lib/home/view.dart
@@ -1,5 +1,7 @@
+import 'package:encrypt_shared_preferences/provider.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:ecloudseal_proj/login/view.dart';
 
 import 'logic.dart';
 
@@ -7,9 +9,39 @@ class HomePage extends StatelessWidget {
   HomePage({super.key});
 
   final HomeLogic logic = Get.put(HomeLogic());
-
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('首頁'),
+        actions: [
+          IconButton(
+            tooltip: '登出',
+            icon: const Icon(Icons.logout),
+            onPressed: () async {
+              final prefs = EncryptedSharedPreferencesAsync.getInstance();
+              await prefs.remove('account');
+              await prefs.remove('password');
+              await prefs.remove('biometric_enrolled');
+              await prefs.remove('biometric_types');
+              Get.offAll(LoginPage());
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text('歡迎登入', style: Theme.of(context).textTheme.titleLarge),
+            const SizedBox(height: 8),
+            Obx(() {
+              return Text('帳號：${logic.account.value}');
+            }),
+          ],
+        ),
+      ),
+    );
   }
 }
+
